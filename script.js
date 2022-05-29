2cbca317389a335f717fd1147413e5feadaebbfd
KEHINDE OLANIYAN <64592116+Kennywhite-star@users.noreply.github.com>
2022-05-29 15:54:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let menuBar = document.querySelector('#menu-bar');
+let navBar = document.querySelector('.nav-bar');
+let header = document.querySelector('.header-2');
+
+menuBar.addEventListener('click', () => {
+    menuBar.classList.toggle('fa-times');
+    navBar.classList.toggle('active');
+});
+
+window.onscroll = () => {
+    menuBar.classList.remove('fa-times');
+    navBar.classList.remove('active');
+    
+    if(window.scrollY > 150){
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+}
+
+let countDate = new Date('june 1, 2022 00:00:00').getTime();
+
+function countDown(){
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerHTML= d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+    countDown();
+},1000);
+
+
